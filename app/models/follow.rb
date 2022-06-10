15a97e55318fb5b40e30c393f2b3b4756d33aaf8
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2022-06-10 15:53:44 -0300

    Doesnt allow to follow yourself

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,10 @@
 class Follow < ApplicationRecord
   validates :followed_user_id, :presence => true, :uniqueness => true
+  validate :doesnt_follow_self
+
+  private
+
+  def doesnt_follow_self
+    errors.add(:base, 'You can\'t follow yourself') if follower_id == followed_user_id
+  end
 end
