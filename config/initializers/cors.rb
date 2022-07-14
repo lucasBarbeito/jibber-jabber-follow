26bcae5b631afd9f6b936afb319f21e50c454034
Georges Stefan <stefangeorges4799@gmail.com>
2022-07-14 17:51:07 -0300

    Adds cors origin value

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://jibber-jabber.brazilsouth.cloudapp.azure.com'
+    origins 'http://jibber-jabber.brazilsouth.cloudapp.azure.com', 'jibber-jabber.brazilsouth.cloudapp.azure.com'
 
     resource "*",
       headers: :any,
