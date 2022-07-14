223d456f2257c21aea4d54cb828177e50e02e346
Georges Stefan <stefangeorges4799@gmail.com>
2022-07-14 19:23:01 -0300

    Fixes cors

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,5 @@ module JibberJabberFollow
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.hosts.clear
   end
 end
